d0572dbeb9c1046ec020785df5a804e4600d5875
Nguyen Anh Khoa <nguyenanhkhoa20055@gmail.com>
2026-05-23 14:01:26 +0700

    Components Part 1

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer className="bg-light text-center text-lg-start">
+            <div className="text-center p-3" style={{backgroundColor: 'rgba(0, 0, 0, 0.2)'}}>
+                © 2024 Copyright:
+                <a className="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+            </div>
+        </footer>
+    );
+}
